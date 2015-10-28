0e296b2e3ee4f7faad54385ecd0aecb2096985dd
juancruz <juancruz@Juan>
2015-10-28 17:22:35 -0300

    class atributo

---
+++ ProyectoBD2/src/proyectobd2/Atributo.java
@@ -0,0 +1,55 @@
+package proyectobd2;
+
+import java.util.Objects;
+
+
+/**
+ *
+ * @author juancruz
+ */
+
+public class Atributo {
+    private String nombre;
+    private String tipo;
+
+    public Atributo(String nombre, String tipo) {
+        this.nombre = nombre;
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Atributo other = (Atributo) obj;
+        if (!Objects.equals(this.nombre, other.nombre)) {
+            return false;
+        }
+        if (!Objects.equals(this.tipo, other.tipo)) {
+            return false;
+        }
+        return true;
+    }
+    
+    
+}
