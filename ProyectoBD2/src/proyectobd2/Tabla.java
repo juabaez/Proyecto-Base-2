b4a0623a4284e58d98447a998e82491132c3215c
juancruz <juancruz@Juan>
2015-11-11 15:00:37 -0300

    Principal methods of passing a Table

--- ProyectoBD2/src/proyectobd2/Tabla.java
+++ ProyectoBD2/src/proyectobd2/Tabla.java
@@ -5,6 +5,10 @@
  */
 package proyectobd2;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.LinkedList;
 
 /**
@@ -55,6 +59,32 @@ public class Tabla {
         this.nombre = nombre;
     }
         
+    public LinkedList<Trigger> listaTrigger(String nombreT, Connection con) throws SQLException{
+        Statement s = con.createStatement();
+        LinkedList<Trigger> lT = new LinkedList();
+        LinkedList<String> nombTrigger = nombreTrigger(nombreT,s);
+        LinkedList<String> condTrigger = new LinkedList();
+        ResultSet rs;
+        String disparador=null;
+        for (int i = 0; i < nombTrigger.size(); i++) {
+            rs = s.executeQuery ("SELECT event_manipulation,action_timing FROM information_schema.triggers where trigger_name ='"+nombTrigger.get(i)+"' order by event_manipulation");
+            while (rs.next()){
+                disparador = rs.getObject("action_timing").toString();
+                condTrigger.add(rs.getObject("event_manipulation").toString());
+            }
+            lT.add(new Trigger(nombreT, condTrigger, disparador));
+        }
+        s.close();
+        return lT;
+    }
     
+    public static LinkedList<String> nombreTrigger(String nom,Statement s) throws SQLException{
+        LinkedList<String> nomb = new LinkedList();
+        ResultSet rs = s.executeQuery("SELECT tgname FROM pg_trigger, pg_class WHERE tgrelid=pg_class.oid AND relname='"+nom+"' AND tgname not like '%RI_ConstraintTrigger_%' order by tgname;");
+        while (rs.next()){
+            nomb.add(rs.getObject("tgname").toString());
+        }
+        return nomb;
+    }
     
 }
--- ProyectoBD2/src/proyectobd2/principal.java
+++ ProyectoBD2/src/proyectobd2/principal.java
@@ -227,34 +227,6 @@ public class principal {
         }
     }*/
     
-    public LinkedList<Trigger> listaTrigger(String nombreT) throws SQLException{
-        Statement s = conexion1.createStatement();
-        LinkedList<Trigger> lT = new LinkedList();
-        LinkedList<String> nombTrigger = nombreTrigger(nombreT,s);
-        LinkedList<String> condTrigger = new LinkedList();
-        ResultSet rs;
-        String disparador=null;
-        for (int i = 0; i < nombTrigger.size(); i++) {
-            rs = s.executeQuery ("SELECT event_manipulation,action_timing FROM information_schema.triggers where trigger_name ='"+nombTrigger.get(i)+"' order by event_manipulation");
-            while (rs.next()){
-                disparador = rs.getObject("action_timing").toString();
-                condTrigger.add(rs.getObject("event_manipulation").toString());
-            }
-            lT.add(new Trigger(nombreT, condTrigger, disparador));
-        }
-        s.close();
-        return lT;
-    }
-    
-    public static LinkedList<String> nombreTrigger(String nom,Statement s) throws SQLException{
-        LinkedList<String> nomb = new LinkedList();
-        ResultSet rs = s.executeQuery("SELECT tgname FROM pg_trigger, pg_class WHERE tgrelid=pg_class.oid AND relname='"+nom+"' AND tgname not like '%RI_ConstraintTrigger_%' order by tgname;");
-        while (rs.next()){
-            nomb.add(rs.getObject("tgname").toString());
-        }
-        return nomb;
-    }
-    
     public LinkedList<Procedure> getprocedures(Connection conexion, String bd) throws SQLException{
         LinkedList<Procedure> result=new LinkedList<Procedure>();        
         DatabaseMetaData metaData = conexion.getMetaData();
